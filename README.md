21451706f8593cc7ee37cd5b03511500b87762d5
Mark Wallace <mark.t.wallace@gmail.com>
2016-05-31 15:10:55 -0700

    Command line, reads stdin

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # jsonpp
 JSON pretty print that minimizes line count while preserving readablity
+
+Written in node.js, v6
+
+Takes JSON from stdin and writes to stdout. E.g.
+
+node jsonpp < ugly.json > pretty.json
+
+I wrote this because my AWS CloudFormation templates were getting very long
+to scroll through. This tightened them up a bit and seemed like a good
+compromise.
---
+++ jsonpp.js
@@ -0,0 +1,37 @@
+const stdin = process.stdin,
+      stdout = process.stdout,
+      inputChunks = []
+
+stdin.resume()
+stdin.setEncoding('utf8')
+
+stdin.on('data', function (chunk) {
+    inputChunks.push(chunk)
+});
+
+stdin.on('end', function () {
+    var inputJSON = inputChunks.join(),
+        parsedData = JSON.parse(inputJSON),
+        outputJSON = prettyPrint(parsedData,'')
+    stdout.write(outputJSON)
+    stdout.write('\n')
+});
+
+function prettyPrint(obj,indent) {
+  const text = JSON.stringify(obj)
+  if(text.length < 80 || typeof obj === 'string') {
+    return text
+  } else if(Object.keys(obj).length === 1) {
+    // To stack object with a  single prop into one line:
+    for(const prop in obj) {
+      return '{"' + prop + '":' + prettyPrint(obj[prop],indent) + '}'
+    }
+  } else {
+    const arr = []
+    for(const prop in obj) {
+      arr.push(indent + '  "' + prop + '":' + prettyPrint(obj[prop],indent+'  '))
+    }
+    return '{\n' + arr.join(',\n') + '\n' + indent + '}'
+    // for smooshed format: return '{\n' + arr.join(',\n') + '}'
+  }
+}
